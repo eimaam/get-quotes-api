ff94dd81dbcc1541b91ff863c97b9388efe6df38
Imam Dahir Dan-Azumi <43689084+eimaam@users.noreply.github.com>
2023-12-30 13:12:33 +0100

    update get all quotes route

--- routes/index.ts
+++ routes/index.ts
@@ -6,6 +6,6 @@ import { getRandom } from "../handlers/getRandom"
 export const v1Router = Router()
 
 v1Router.get('/check', (_req: Request, res:Response) => res.status(200).json({ message: 'API running'}))
-v1Router.get('/', getAllQuotes)
+v1Router.get('/quotes', getAllQuotes)
 v1Router.get('/random', getRandom)
 v1Router.get('/:filterType/:filterValue', getByFilter)
